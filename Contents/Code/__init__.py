93b43f896d416e04566cdb5a5df380d242a787aa
Pierre <p.dellanave@gmail.com>
2010-11-30 23:14:25 -0800

    Plugin rewrite for v2 Framework

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,88 +1,93 @@
-from PMS import Plugin, Log, XML, HTTP, JSON, Prefs
-from PMS.MediaXML import *
-from PMS.FileTypes import PLS
-from PMS.Shorthand import _L, _R, _E, _D
-import gdata.youtube, gdata.youtube.service
 import re
+
+YOUTUBE_STANDARD_FEEDS = 'http://gdata.youtube.com/feeds/api/standardfeeds'
+YOUTUBE_STANDARD_TOP_RATED_URI = '%s/%s' % (YOUTUBE_STANDARD_FEEDS, 'top_rated')
+YOUTUBE_STANDARD_MOST_VIEWED_URI = '%s/%s' % (YOUTUBE_STANDARD_FEEDS, 'most_viewed')
+YOUTUBE_STANDARD_RECENTLY_FEATURED_URI = '%s/%s' % (YOUTUBE_STANDARD_FEEDS, 'recently_featured')
+YOUTUBE_STANDARD_WATCH_ON_MOBILE_URI = '%s/%s' % (YOUTUBE_STANDARD_FEEDS, 'watch_on_mobile')
+YOUTUBE_STANDARD_TOP_FAVORITES_URI = '%s/%s' % (YOUTUBE_STANDARD_FEEDS, 'top_favorites')
+YOUTUBE_STANDARD_MOST_RECENT_URI = '%s/%s' % (YOUTUBE_STANDARD_FEEDS, 'most_recent')
+YOUTUBE_STANDARD_MOST_DISCUSSED_URI = '%s/%s' % (YOUTUBE_STANDARD_FEEDS, 'most_discussed')
+YOUTUBE_STANDARD_MOST_LINKED_URI = '%s/%s' % (YOUTUBE_STANDARD_FEEDS, 'most_linked')
+YOUTUBE_STANDARD_MOST_RESPONDED_URI = '%s/%s' % (YOUTUBE_STANDARD_FEEDS, 'most_responded')
+YOUTUBE_QUERY = "http://gdata.youtube.com/feeds/api/videos?q=%s"
+
+DEVELOPER_KEY = "AI39si7PodNU93CVDU6kxh3-m2R9hkwqoVrfijDMr0L85J94ZrJFlimNxzFA9cSky9jCSHz9epJdps8yqHu1wb743d_SfSCRWA"
 
 PLUGIN_PREFIX       = "/video/youtube"
 YT_FMT              = [34, 18, 35, 22, 37]
 
-yt_service          = None
 yt_videoURL         = "http://www.youtube.com/get_video?asv=3&video_id="
+
+ART = 'art-default.jpg'
+ICON = 'icon-default.png'
 
 ####################################################################################################
 
 def Start():
-  Plugin.AddRequestHandler(PLUGIN_PREFIX, HandleRequest, "YouTube", "icon-default.png", "art-default.png")
-  Plugin.AddViewGroup("InfoList", viewMode="InfoList", contentType="items")  
-  Plugin.AddViewGroup("List", viewMode="List", contentType="items") 
-
-  global yt_service
-  yt_service = gdata.youtube.service.YouTubeService()
-  yt_service.developer_key = "AI39si7PodNU93CVDU6kxh3-m2R9hkwqoVrfijDMr0L85J94ZrJFlimNxzFA9cSky9jCSHz9epJdps8yqHu1wb743d_SfSCRWA"
-  yt_service.client_id = "ytapi-Plex-YouTubePlugin-ei1mp311-0"
+  Plugin.AddPrefixHandler(PLUGIN_PREFIX, MainMenu, "YouTube", ICON, ART)
+  Plugin.AddViewGroup("InfoList", viewMode="InfoList", mediaType="items")  
+  Plugin.AddViewGroup("List", viewMode="List", mediaType="items") 
+  MediaContainer.title1 = 'YouTube'
+  MediaContainer.content = 'Items'
+  MediaContainer.art = R(ART)
+  DirectoryItem.thumb = R(ICON)
+  HTTP.CacheTime = 3600
+  HTTP.Headers["User-agent"] = "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; en-US; rv:1.9.2.12) Gecko/20101026 Firefox/3.6.12"  
   
 ####################################################################################################
-def parseFeed(feed,dir):
-  for video in feed.entry:
-    id = PLUGIN_PREFIX + "/play^" + _E(video.media.player.url)
-    title = video.media.title.text
-    summary = video.media.description.text
-    duration = str(int(video.media.duration.seconds)*1000)
-    thumb = video.media.thumbnail[0].url
-    dir.AppendItem(VideoItem(id, title, summary, duration, thumb))
+def getThumb(path):
+  return DataObject(HTTP.Request(path).content,'image/jpeg')
+  
+def parseFeed(sender = None,url=''):
+  dir = MediaContainer(viewGroup="InfoList")  
+  
+  if url.find('?')>0:
+    url = url + "&alt=json"
+  else:
+    url = url + "?alt=json"
+
+  try:
+    rawfeed = JSON.ObjectFromURL(url,encoding = 'utf-8')
+    for video in rawfeed['feed']['entry']:
+      id = video['media$group']['media$player'][0]['url']
+      Log(id)
+      title = video['title']['$t']
+      summary = video['content']['$t']
+      duration = str(int(video['media$group']['yt$duration']['seconds'])*1000)
+      thumb = video['media$group']['media$thumbnail'][0]['url']
+      dir.Append(Function(VideoItem(PlayVideo, title, subtitle='', summary=summary, duration=duration, thumb=Function(getThumb, path = thumb)), address = id))
+  
+    return dir
+  except:
+    return MessageContainer("Error","This query did not return any result") 
+    
+def MainMenu():
+  dir = MediaContainer(viewGroup="List")  
+  dir.Append(Function(DirectoryItem(SubMenu, L("Today")),category = 'today'))
+  dir.Append(Function(DirectoryItem(SubMenu, L("This Week"), L("This Week")),category = 'this_week'))
+  dir.Append(Function(DirectoryItem(SubMenu, L("This Month"), L("This Month")),category = 'this_month'))
+  dir.Append(Function(DirectoryItem(SubMenu, L("All Time"), L("All Time")),category = 'all_time'))
+  dir.Append(Function(DirectoryItem(parseFeed, L("Most Recent"), ""),url= YOUTUBE_STANDARD_MOST_RECENT_URI))
+  dir.Append(Function(InputDirectoryItem(Search,L("Search YouTube"), L("Search YouTube"), L("Search YouTube"), thumb=R("search.png"))))
   return dir
 
-def HandleRequest(pathNouns, count):
-  global yt_service
-  try:
-    title2 = pathNouns[count-1].split("||")[1]
-    pathNouns[count-1] = pathNouns[count-1].split("||")[0]
-  except:
-    title2 = ""
-    
-  if count > 1: vg="InfoList"
-  else: vg="List"
-    
-  dir = MediaContainer("art-default.png", viewGroup=vg, title1="YouTube", title2=title2)  
-  if count == 0:
-    dir.AppendItem(DirectoryItem("today||" + _L("Today"), _L("Today"), thumb=_R("icon-default.png")))
-    dir.AppendItem(DirectoryItem("this_week||" + _L("This Week"), _L("This Week"), thumb=_R("icon-default.png")))
-    dir.AppendItem(DirectoryItem("this_month||" + _L("This Month"), _L("This Month"), thumb=_R("icon-default.png")))
-    dir.AppendItem(DirectoryItem("all_time||" + _L("All Time"), _L("All Time"), thumb=_R("icon-default.png")))
-    dir.AppendItem(DirectoryItem("feed^YOUTUBE_STANDARD_MOST_RECENT_URI||" + _L("Most Recent"), _L("Most Recent"), thumb=_R("icon-default.png")))
-    dir.AppendItem(SearchDirectoryItem("search||" + _L("Search YouTube"), _L("Search YouTube"), _L("Search YouTube"), _R("search.png")))
-    
-  elif count == 1 and pathNouns[0] in ['today', 'this_week', 'this_month', 'all_time']:
-    dir.AppendItem(DirectoryItem(("feed^YOUTUBE_STANDARD_MOST_VIEWED_URI^%s||" % pathNouns[0]) + _L("Most Viewed"), _L("Most Viewed"), thumb=_R("icon-default.png")))
-    dir.AppendItem(DirectoryItem(("feed^YOUTUBE_STANDARD_TOP_RATED_URI^%s||" % pathNouns[0]) + _L("Top Rated"), _L("Top Rated"), thumb=_R("icon-default.png")))
-    dir.AppendItem(DirectoryItem(("feed^YOUTUBE_STANDARD_MOST_DISCUSSED_URI^%s||" % pathNouns[0]) + _L("Most Discussed"), _L("Most Discussed"), thumb=_R("icon-default.png")))
-    
-  elif pathNouns[0].startswith("feed") and count < 2:
-    dir = parseFeed(yt_service.GetYouTubeVideoFeed(pathNouns[0].split("^")[1]), dir)
-    
-  elif count > 1 and pathNouns[1].startswith("feed"):
-    params = pathNouns[1].split("^")
-    dir = parseFeed(yt_service.GetYouTubeVideoFeed(params[1], params[2]), dir)
-    
-  elif pathNouns[0].startswith("search") and count < 3:
-    if count > 1:
-      query = pathNouns[1]
-    if count > 2:
-      for i in range(2, len(pathNouns)): query += "/%s" % pathNouns[i]
-    yt_query = gdata.youtube.service.YouTubeVideoQuery()
-    yt_query.vq = query # the term(s) that you are searching for
-    yt_query.orderby = 'relevance'    # how to display the results
-    yt_query.max_results = '40'        # number of results to retrieve
-    dir = parseFeed(yt_service.YouTubeQuery(yt_query),dir)
-    if dir.ChildCount() == 0:
-      dir.AppendItem(DirectoryItem("%s/search" % PLUGIN_PREFIX, "(No Results)", ""))
+def SubMenu(sender, category):
+  dir = MediaContainer(viewGroup="InfoList")  
+  dir.Append(Function(DirectoryItem(parseFeed, L("Most Viewed"), ""),url = (YOUTUBE_STANDARD_MOST_VIEWED_URI+"?time=%s" %category)))
+  dir.Append(Function(DirectoryItem(parseFeed, L("Top Rated"), ""),url = (YOUTUBE_STANDARD_TOP_RATED_URI+"?time=%s" % category)))
+  dir.Append(Function(DirectoryItem(parseFeed, L("Most Discussed"), ""),url = (YOUTUBE_STANDARD_MOST_DISCUSSED_URI+"?time=%s" % category)))
+  return dir
+      
+def Search(sender, query = None):
+  return parseFeed(url=YOUTUBE_QUERY % query.replace(' ','+'))
    
-  elif pathNouns[-1].startswith("play"):
-    address = _D(pathNouns[-1].split("^")[1])
-    ytPage = HTTP.Get(address)
+def PlayVideo(sender,address):
+    ytPage = HTTP.Request(address).content
     
+    Log(address)
+    Log(ytPage)
+
     try:
       t = re.findall('&t=(.[^&"]{10,})', ytPage, re.IGNORECASE)[0]
     except:
@@ -90,10 +95,9 @@ def HandleRequest(pathNouns, count):
         t = re.findall('"t": "([^"]+)"', ytPage)[0]
       except:
         t = ''
-    #Log.Add(t)
 
     v = address[address.find('v=')+2:address.find('&feature')] #
-    Log.Add(v)
+    Log(v)
     
     fmt_list = re.findall('&fmt_list=([^&]+)', ytPage)[0]
     fmts = re.findall('([0-9]+)[^,]*', fmt_list)
@@ -110,7 +114,6 @@ def HandleRequest(pathNouns, count):
           fmt = 5
 
     u = yt_videoURL + v + "&t=" + t + "&fmt=" + str(fmt)
-    return Plugin.Redirect(u)
+    return Redirect(u)
 
-  return dir.ToXML()
 ####################################################################################################
